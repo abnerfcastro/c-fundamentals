16493a3ab6e0bef4883b689aea943c868e0cf678
Abner Castro <abnerfcastro@gmail.com>
2016-07-16 16:02:07 -0300

    Advanced helloworld using function pointers

---
+++ miscellaneous/advanced-helloworld/Readme.md
@@ -0,0 +1,1 @@
+`advanced-helloworld.c` is a multilingual _helloworld_ program that uses the concept of *function pointers*. If you're not familiar to function pointers, this will be quite helful for you.
---
+++ miscellaneous/advanced-helloworld/main.c
@@ -0,0 +1,43 @@
+/*
+ * Author: Abner Castro
+ * Date: July 16th, 2016
+ * Description:
+ *      A multilingual hello world program using function pointers
+ */
+
+#include <stdio.h>
+
+// The main helloworld function
+void helloworld(const char*, void (*)(const char*));
+
+void english(const char*);
+void portuguese(const char*);
+void dutch(const char*);
+
+int main()
+{
+    helloworld("Abner", english);
+    helloworld("Abner", portuguese);
+    helloworld("Abner", dutch);
+    return 0;
+}
+
+void helloworld(const char *name, void (*language)(const char*))
+{
+    language(name);
+}
+
+void english(const char *name)
+{
+    printf("Hello World! Welcome, %s\n", name);
+}
+
+void portuguese(const char *name)
+{
+    printf("Ola mundo! Bem-vindo, %s\n", name);
+}
+
+void dutch(const char* name)
+{
+    printf("Hallo Wereld! Welkom, %s\n", name);
+}
